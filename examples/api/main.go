ec1af20dfc349be5f83294a81824321d067b494f
Terrence <tpryan@google.com>
2023-03-08 10:45:38 -0800

    chore: adding godoc

--- examples/api/main.go
+++ examples/api/main.go
@@ -12,6 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// main provides an API that serves up random colors
 package main
 
 import (
